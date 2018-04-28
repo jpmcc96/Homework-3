ee3ee86746aa08b88ecf8dc6a9a1b901fb20324a
Akash Kumar <34189485+Kujaakhi2017@users.noreply.github.com>
2018-04-27 20:07:43 -0400

    Parking meter version 1

--- ParkingMeter.java
+++ ParkingMeter.java
@@ -1,1 +1,43 @@
-# ParkingMeter.java
+/**
+ *Simulates a parking meter.
+ *@author Akash Kumar
+ *@version 1.0
+ *@date 2018/04/27 
+ */
+public class ParkingMeter
+{
+  private int minutesPurchased;
+  
+  /**
+   *This default constructor
+    initailizes minutesPurchased to 60.
+   */
+  public ParkingMeter()
+  {
+    this(60);
+  }
+  
+  /**
+   *This constructor sets the
+    number of minutes purchased.
+   *@param minutes the number of minutes
+    purchased.
+   */
+  public ParkingMeter(int minutes)
+  {
+    if(minutes >= 0)
+      minutesPurchased = minutes;
+    else
+      minutesPurchased = 0;
+  }
+  
+  /**
+   *Returns the number of minutes
+    purchased.
+   *@return the minutes purchased.
+   */
+  public int getMinutes()
+  {
+    return minutesPurchased;
+  }
+}
