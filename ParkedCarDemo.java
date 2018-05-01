033504549a67222de87d3b3ec3bd2b52c56d0436
Akash Kumar <34189485+Kujaakhi2017@users.noreply.github.com>
2018-04-30 20:19:36 -0400

    just a rough sketch....

--- ParkedCarDemo.java
+++ ParkedCarDemo.java
@@ -1,1 +1,34 @@
+//To test homework 3 parking project.
+//By Akash Kumar, James McCarthy, Keyshawn Wright
 
+public class ParkingDemo
+{
+  public static void main(String[] args)
+  {
+    //car object.
+    String make = "Tesla",
+          model = "S",
+          color = "red",
+       licenNum = "4GET OIL";
+    int minutesParked = 120;
+    ParkedCar car1 = new ParkedCar();
+    
+    //meter object.
+    int minutesPurchased = 90;
+    ParkingMeter meter1 = new ParkingMeter(minutesPurchased);
+    
+    //police object.
+    String policeName = "Steve Joblez";
+    String badgeNum = "007";
+    PoliceOfficer officer1 = new PoliceOfficer(policeName, badgeNum);
+    
+    //ticket object.
+    ParkingTicket ticket1 = officer1.inspectCar(car1, meter1);
+    
+    //output results.
+    if(ticket1 != null)
+      System.out.println(ticket1);
+    else
+      System.out.println("That " + make + " sure looks neat!");
+  }
+}
