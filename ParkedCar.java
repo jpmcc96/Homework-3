ef23e53c8409f8594df987f21be6bf935aa65fa5
TsukiPrisma <37780811+TsukiPrisma@users.noreply.github.com>
2018-05-02 17:20:02 -0400

    Update ParkedCar.java

--- ParkedCar.java
+++ ParkedCar.java
@@ -1,1 +1,49 @@
-# ParkedCar.java
+public class ParkedCar
+{
+     private String make, model, color, licenseNum;
+     private int minutesParked;
+   
+   public ParkedCar(String make, String model, String color, String licenseNum)
+   {
+      this(make, model, color, licenseNum ,0);
+   }
+   
+   public ParkedCar(String make, String model, String color, String licenseNum, int minutesParked)
+   {
+      this.make = make;
+      this.model = model;
+      this.color = color;
+      this.licenseNum = licenseNum;
+      this.minutesParked = minutesParked;
+   }
+   
+   public void setMinutesParked(int minutesParked)
+   {
+      this.minutesParked = minutesParked;
+   }
+   
+   public String getMake()
+   {
+      return make;
+   }
+   
+   public String getModel()
+   {
+      return model;
+   }
+   
+   public String getColor()
+   {
+      return color;
+   }
+   
+   public String getLicenseNum()
+   {
+      return licenseNum;
+   }
+   
+   public int getMinutesParked()
+   {
+      return minutesParked;
+   }
+}
