bab6ffd19d0af963e147b8285d63973cb6611263
jpmcc96 <37561603+jpmcc96@users.noreply.github.com>
2018-05-03 16:07:33 -0400

    Update PoliceOfficer.java
    I may have mixed up some classes with each other, send me a message in Slack if you run into any issues.

--- PoliceOfficer.java
+++ PoliceOfficer.java
@@ -1,1 +1,73 @@
 # PoliceOfficer.java
+public class PoliceOfficer
+{
+   private String name;
+   private String badgeNum;
+    
+   public PoliceOfficer(String name, String badgeNum)
+   {
+      this.name = name;
+      this.badgeNum = badgeNum;
+   }
+   
+   public inspectCar(ParkedCar, status)
+   {
+      this.ParkedCar = ParkedCar;
+      this.status = status;
+      
+      if(this.status = true)
+      {
+         parkingTicket = new ParkingTicket();
+      }
+      
+      else
+      {
+         System.out.println("Car is not parked illegally"); 
+      }
+      
+   }
+   
+   private boolean hasTimeExpired(int illegal)
+   {
+      
+      if(illegal != null)
+      {
+         status = true;      
+      }
+      
+      else
+      {
+         status = false;
+      }
+      
+      return status;
+   }
+   
+   public int getIllegalMinutes(car, meter)
+   {
+      int illegal = null;
+      
+      if(minutesPurchased < ParkingMeter)
+      {
+         illegal = (minutesPurchased - ParkingMeter);   
+      }
+      else
+      {
+         illegal = null;
+      }
+      return illegal;
+   }
+   
+   public String getOfficerName(name)
+   {
+      officerName = name;
+      return officerName;
+   }
+   
+   public String getBadgeNum(badgeNum)
+   {
+      this.badgeNum = badgeNum;
+      return badgeNum;
+   }
+   
+}
